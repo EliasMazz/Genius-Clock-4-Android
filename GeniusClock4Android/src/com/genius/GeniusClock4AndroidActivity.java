1a33e4d3ba2b5154c7f1ba0d128706de100c2de8
Elias <elias.mazzocco@gmail.com>
2012-05-03 18:55:09 -0300

    Primeiro commit, Descrição foi feito o layout com botões

---
+++ GeniusClock4Android/src/com/genius/GeniusClock4AndroidActivity.java
@@ -0,0 +1,32 @@
+package com.genius;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class GeniusClock4AndroidActivity extends Activity {
+    
+	ArrayList<Integer> sequencia = new ArrayList<Integer>();
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+    }
+    
+    public void Btn1(View view){
+    	
+    }
+    public void Btn2(View view){
+    	
+    }
+    public void Btn3(View view){
+    	
+    }
+    public void Btn4(View view){
+    	
+    }
+}
